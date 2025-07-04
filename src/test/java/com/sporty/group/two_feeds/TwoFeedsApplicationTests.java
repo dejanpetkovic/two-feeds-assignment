461b5d24a3d2e98c7d6b9e98acbc8b239524e624
dejanpetkovic <dekietf@gmail.com>
2025-07-04 10:10:54 +0200

    Initial commit

---
+++ src/test/java/com/sporty/group/two_feeds/TwoFeedsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sporty.group.two_feeds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TwoFeedsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
